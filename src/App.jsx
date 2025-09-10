d1f2f4bfdb9a7b984c4d47697ba1b8d920fc06ae
vanellopefreedom <freedomwrldd@gmail.com>
2025-09-10 16:07:46 +0100

    fixing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Card from './Card'
-import Messages from './Messages'
-import Profile from './Profile'
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Card from './Card';
+import Messages from './Messages';
+import Profile from './Profile';
+
 const App = () => {
   return (
     <Router>
-      <>
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={< Card />} />
-          <Route path="/play" element={<Card />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/profile" element={<Profile />} /> 
-        </Routes>
-      </>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Card />} />
+        <Route path="/play" element={<Card />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
